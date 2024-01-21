4652425f5a7a99f693c0ef45220ac8f7b9301284
Mobina Abforoosh <mobinaabf@gmail.com>
2024-01-21 11:31:29 +0330

    Considering the condition for loading the image

--- annotate.cpp
+++ annotate.cpp
@@ -1,6 +1,14 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
+using namespace std;
 
 int main(){
     Mat image = imread("sample.jpg");
+
+    if (image.empty()){
+        cerr<<"error loading image \n";
+        return -1;
+    }
+
+    
 }
