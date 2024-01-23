88e798b578d4f0e13a9fa03272acbb66ffa52667
Mobina Abforoosh <mobinaabf@gmail.com>
2024-01-23 11:06:48 +0330

    Creating a temporary matrix that is a copy of the image matrix and changes are made on it and getting the character from the input to perform the required action

--- annotate.cpp
+++ annotate.cpp
@@ -10,5 +10,24 @@ int main(){
         return -1;
     }
 
+    while (1){
+        Mat tempImage = image.clone();
+        drawShapes(tempImage);
+        imshow("Image", tempImage);
+
+        char key = waitKey(1);
+
+        if (key == 'r') {
+            shapeType = 0;  // Set to rectangle
+        } else if (key == 'c') {
+            shapeType = 1;  // Set to circle
+        } else if (key == ' ') {
+            imwrite("labeled_image.jpg", tempImage);
+            std::cout << "Labeled image saved." << std::endl;
+        } else if (key == 27) {
+            // Pressing Esc key breaks the loop and exits the program
+            break;
+        }
+    }
     
 }
