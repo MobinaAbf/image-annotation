4ea6800feaa7b2dc6370bb431218c0afe52bbf31
Mobina Abforoosh <mobinaabf@gmail.com>
2024-01-23 21:49:23 +0330

    Add the possibility of saving coordinates in the file

--- annotate.cpp
+++ annotate.cpp
@@ -1,19 +1,50 @@
 #include <opencv2/opencv.hpp>
+#include <fstream>
 using namespace cv;
 using namespace std;
 
 bool drawing = false;
 int shapeType = 0;  // 0 for rectangle, 1 for circle
-Rect selectedRectangle;//strust of rectangle with width & height 
-Point2f circleCenter;//float
+Rect selectedRectangle; // structure of rectangle with width & height
+Point2f circleCenter;    // float
 int radius = 0;
 
 void drawShapes(Mat &image) {
     if (shapeType == 0) {
         rectangle(image, selectedRectangle, Scalar(0, 255, 0), 2);
+
+        string rectCoords = "Top_Left: (" + to_string(selectedRectangle.x) + ", " +
+        to_string(selectedRectangle.y) + ") Down_Right: (" +   
+        to_string(selectedRectangle.x + selectedRectangle.width) + ", " +
+        to_string(selectedRectangle.y + selectedRectangle.height) + ")";
+        putText(image, rectCoords, Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1);
+
     } else if (shapeType == 1) {
         circle(image, circleCenter, radius, Scalar(0, 0, 255), 2);
+
+        string circleCoords = "Center: (" + to_string(circleCenter.x) + ", " +
+        to_string(circleCenter.y) + ") Radius: " + to_string(radius);
+
+        putText(image, circleCoords, Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
     }
+}
+
+void saveCoordinatesToFile(const string &filename, int shapeType, const Rect &rect, const Point2f &center, int radius) {
+    ofstream file(filename, ios::app); 
+
+    if (!file.is_open()) {
+        cerr << "Error opening file for writing!" << endl;
+        return;
+    }
+
+    if (shapeType == 0) {
+        file << "Rectangle, Top_Left: (" << rect.x << ", " << rect.y << "), Down_Right: ("
+             << rect.x + rect.width << ", " << rect.y + rect.height << ")" << endl;
+    } else if (shapeType == 1) {
+        file << "Circle, Center: (" << center.x << ", " << center.y << "), Radius: " << radius << endl;
+    }
+
+    file.close();
 }
 
 void mouseCallback(int event, int x, int y, int flags, void *userdata) {
@@ -39,15 +70,17 @@ void mouseCallback(int event, int x, int y, int flags, void *userdata) {
     }
 }
 
-int main(){
+int main() {
     Mat image = imread("sample.jpg");
-
-    if (image.empty()){
-        cerr<<"error loading image \n";
+    if (image.empty()) {
+        cerr << "Error loading image!" << endl;
         return -1;
     }
 
-    while (1){
+    namedWindow("Image");
+    setMouseCallback("Image", mouseCallback);
+
+    while (true) {
         Mat tempImage = image.clone();
         drawShapes(tempImage);
         imshow("Image", tempImage);
@@ -55,12 +88,13 @@ int main(){
         char key = waitKey(1);
 
         if (key == 'r') {
-            shapeType = 0;  // Set to rectangle
+            shapeType = 0; // Set to rectangle
         } else if (key == 'c') {
-            shapeType = 1;  // Set to circle
+            shapeType = 1; // Set to circle
         } else if (key == ' ') {
             imwrite("labeled_image.jpg", tempImage);
-            std::cout << "Labeled image saved." << std::endl;
+            saveCoordinatesToFile("coordinates.txt", shapeType, selectedRectangle, circleCenter, radius);
+            cout << "Labeled image and coordinates saved." << endl;
         } else if (key == 27) {
             // Pressing Esc key breaks the loop and exits the program
             break;
@@ -69,5 +103,4 @@ int main(){
 
     destroyAllWindows();
     return 0;
-    
 }
